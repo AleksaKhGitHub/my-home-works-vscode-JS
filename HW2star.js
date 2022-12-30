bfb33b9d6b36681ffdb49f3c73150f530cd09837
AleksandraKhouryna <103449946+AleksaKhouryna@users.noreply.github.com>
2022-12-30 16:50:26 +0300

    Add files with home work 2 (js)

---
+++ HW2star.js
@@ -0,0 +1,148 @@
+
+// 1*
+
+// let age = 10;
+// let age_2 = 18;
+// let age_3 = 60;
+
+
+// const checkAge = function(age){
+
+//     if(+(age)) {
+
+
+//     if (age < age_2) {
+    
+//     console.log("You do not have access cause your age is" + age + "It is less then" + age_2)
+
+//     } else if (age >= age_2 && age < 60 ) {
+
+//     console.log("Welcome!")
+
+//     } else if (age  > age_3) {
+ 
+//     console.log("Keep calm and look Culture channel")
+    
+//     }  else {
+//     console.log("Technical work")
+//     }
+
+// } else { console.log(`not integer value`)}    
+// }
+
+
+// checkAge(17)
+// checkAge(18)
+// checkAge(61)
+
+
+
+// 2*
+
+// let age = 10;
+// let age_2 = 18;
+// let age_3 = 60;
+
+
+// const checkAge = function(age){
+
+//     if(!isNaN(age)){
+//         console.log("this is mistake")}
+     
+
+//     if (age < age_2) {
+    
+//     console.log("You do not have access cause your age is" + age + "It is less then" + age_2)}
+
+//     else if (age >= age_2 && age < 60 ) {
+
+//     console.log('Welcome!')}
+    
+//     else if (age  > age_3) {
+ 
+//     console.log('Keep calm and look Culture channel')} 
+      
+// } 
+
+// checkAge(17)
+// checkAge(18)
+// checkAge(61)
+// checkAge('dfdf')
+// checkAge('-22')
+// checkAge('d22ddc')
+// checkAge('55')
+
+// 3**
+
+// let age = 10;
+// let age_2 = 18;
+// let age_3 = 60;
+
+
+// const checkAge = function(age){
+
+//     if(isNaN(age)){
+//         console.log("this is mistake")}
+     
+
+//     if (age < age_2) {
+    
+//     console.log("You do not have access cause your age is" + age + "It is less then" + age_2)}
+
+//     else if (age >= age_2 && age < 60 ) {
+
+//     console.log('Welcome!')}
+    
+//     else if (age  > age_3) {
+ 
+//     console.log('Keep calm and look Culture channel')} 
+      
+// } 
+
+// checkAge(17)
+// checkAge(18)
+// checkAge(61)
+// checkAge('dfdf')
+// checkAge('-22')
+// checkAge('d22ddc')
+// checkAge('55')
+
+
+// 4*** + file html
+
+
+let age = 10;
+let age_2 = 18;
+let age_3 = 60;
+
+
+const checkAge = function(age){
+
+    if(isNaN(age)){
+        console.log("this is mistake")}
+     
+
+    if (age < age_2) {
+    
+    console.log("You do not have access cause your age is" + " " + age + " " + "It is less then" + " " + age_2)}
+
+    else if (age >= age_2 && age < 60 ) {
+
+    console.log('Welcome!')}
+    
+    else if (age  > age_3) {
+ 
+    console.log('Keep calm and look Culture channel')} 
+      
+} 
+
+checkAge(17)
+checkAge(18)
+checkAge(61)
+checkAge('dfdf')
+checkAge('-22')
+checkAge('d22ddc')
+checkAge('55')
+
+let agePrompt = prompt('Enter age')
+checkAge(agePrompt)
